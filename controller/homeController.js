f3f0d2e5067ffb2a92a9962e24b0c09a7a262ffe
Dat Nguyen <dathhcc2@gmail.com>
2023-09-08 17:30:55 +0700

    continue

--- controller/homeController.js
+++ controller/homeController.js
@@ -5,6 +5,25 @@ class HomeController {
   index(req, res) {
     res.json(Users);
   }
+
+  //   GET /:id
+  detail(req, res) {
+    const param = req.params.id;
+    const filterId = Users.filter((user) => user.id == param);
+    res.json(filterId);
+  }
+  //   GET /role
+  role(req, res) {
+    const filterRole = Users.filter(
+      (user) => user.role == 2 && user.info.phone !== ""
+    );
+    res.json(filterRole);
+  }
+  //   GET /status
+  status(req, res) {
+    const filterStatus = Users.filter((user) => user.status == "active");
+    res.json(filterStatus);
+  }
 }
 
 module.exports = new HomeController();
--- router/home.js
+++ router/home.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const homeController = require("../controller/homeController");
+router.get("/active", homeController.status);
+router.get("/role", homeController.role);
+router.get("/:id", homeController.detail);
 router.get("/", homeController.index);
 module.exports = router;
